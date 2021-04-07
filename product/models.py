6418ad54fa99c528eef8ec04806ae3ba0780d52d
IrinaSidelnikova <irisha_sidelnikova@mail.ru>
2021-04-07 21:44:20 +0600

    Add Django project

---
+++ product/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, primary_key=True)
+
+    def __str__(self):
+        return self.name
+# варианты on_delete
+# CASCADE - при удалении категории, ядалятся все продукты с этой категориии
+# SET_NULL - при удалении категории, значеник поля Category для связанных продуктов станет пустым
+# SET_DEFAULT - при удалении категории, значение поля Category в связанных продуктах заменяется на дефолтное
+# PROTECTED - не дает удалить категорию, если в ней есть продукты
+# RESTRICT - не дает удалить категорию, если в ней есть продукты
+# NO_NOTHING - ничего не делает
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    descripiton = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='products')
+
+    def __str__(self):
+        return self.title
+
+
+# ORM (Object-Relation Mapping)
+# связывает классы в каком-то языке и таблицы в базе данных
+
